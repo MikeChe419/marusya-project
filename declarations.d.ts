dce752850a05b6a4b6b5f5cf00b06efeb6f6f539
Mike <amenhottep21@gmail.com>
2025-05-02 19:45:27 +0300

    add declare

---
+++ declarations.d.ts
@@ -0,0 +1,4 @@
+declare module '*.module.scss' {
+    const classses: {[key: string]: string};
+    export default classses
+}
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,14 +1,20 @@
 import React from "react"
 import { FC } from "react"
-import  styles from './Header.module.scss'
+import  styles from  './Header.module.scss'
 
 
 const Header:FC = () => {
     return (
-            <header>
+        <div className={styles.container}>
+            <header className={styles.headerContainer}>
                 <h1 className={styles.header}>хИДЕР</h1>
+                <div className={styles.headerWrap}>Проверка на переполнение когда текст очень длинный и его нужно корректно отобразить</div>
+                {/* <h1 className={styles.header}>хИДЕР</h1> */}
             </header>
+          
+        </div>
     )
 }
 
 export default Header
+
