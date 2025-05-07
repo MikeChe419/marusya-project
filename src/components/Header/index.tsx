75de0912cb8684125948872ab19d1d4d3006f7f2
lyakh <lyakh@npc-ses.ru>
2025-05-07 12:20:53 +0300

    add styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,17 @@
-import React from "react"
-import { FC } from "react"
+import React, { FC } from "react"
+// import { NavLink, useNavigate } from "react-router-dom";
+// import { RouteEnum } from "../../config/constants";
 import  styles from  './Header.module.scss'
 import logoImg from '../../assets/images/logo.svg';
 
 
 const Header:FC = () => {
+    // const navigate = useNavigate()
+
     return (
-
+       
             <header className={styles.headerContainer}>
-                <img src={logoImg} alt="маруся" />
-                <h1 className={styles.header}>маруся</h1>
-
+                <img src={logoImg} alt="маруся"  />
             </header>
           
 
