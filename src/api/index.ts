b3df509f9a997c6df7bc98fde31ef980fcebaebe
Mikhail <amenhottep21@gmail.com>
2025-08-06 17:09:26 +0300

    add class api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: 'https://cinemaguide.skillbox.cc/'
-})
+// export default axios.create({
+//     baseURL: 'https://cinemaguide.skillbox.cc/'
+// })
+
+class Api {
+    constructor() {
+        this._baseURL =  'https://cinemaguide.skillbox.cc/',
+        this._headers = {
+            'content-type': 'application/json'
+        }
+    }
+
+    signIn(login, password) {
+        return axios.post(`${this._baseURL}auth/login`, {login, password})
+       
+    }
+}
+
+const api = new Api()
+
+export default api
